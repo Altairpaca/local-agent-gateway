62d71165a85840020ed38d6009013586aea11495
Altair Li <137127498+Altairpaca@users.noreply.github.com>
2026-09-05 14:35:56 +0800

    feat: export reference adapter kit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./adapter.js";
 export * from "./contracts.js";
 export * from "./dispatcher.js";
+export * from "./reference-adapter.js";
 export * from "./registry.js";
 export { authorize } from "./policy.js";
 export { canonicalJson, createReceipt, sha256Canonical, verifyReceipt } from "./receipt.js";
